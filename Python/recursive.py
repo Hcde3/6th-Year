6fa379aca7f373b227784011f835e7542695bcc9
Harry Daniel <145117624+Hcde3@users.noreply.github.com>
2024-10-09 14:16:59 +0100

    Add files via upload

---
+++ Python/recursive.py
@@ -0,0 +1,17 @@
+def listadd(l):
+    n = l.pop(0)
+    if len(l) > 1:return n + listadd(l)
+    else: return n + l[0]
+
+def integersum(i):
+    if i>1:return i + integersum(i-1)
+    else: return 1
+
+def has_digit(s):
+    if s[0].isdigit():return True
+    elif len(s) == 1: return False
+    else: return has_digit(s[1:])
+    
+print(listadd([1,2,3,4]))
+print(integersum(3))
+print(has_digit("gsgsgxg1"))
