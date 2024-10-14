4778cdface8d04928a468c148ebf9cae7ef699df
Harry Daniel <145117624+Hcde3@users.noreply.github.com>
2024-10-14 11:57:53 +0100

    Add files via upload

---
+++ Python/DT.py
@@ -0,0 +1,25 @@
+import random
+Definitions = {}
+Definitions["Biology"] = {}
+Definitions["Physics"] = {}
+Definitions["Biology"]["Genetics"] = {"DNA":"Deoxyribosenucleic acid"}
+Definitions["Physics"]["Electricity"] = {"Voltage":"V = IR"}
+
+print("""_________________________ Definitions Tester _________________________
+
+
+__DEFENITION___________
+(Pd) Practice a Defenition
+
+__TOPIC________________
+(Pt) Practice a Topic
+(Tt) Test a Topic
+
+__SUBJECT________________
+(Ps) Practice a Subject
+(Ts) Test a Subject
+
+__FILE_________________
+(I) Input a Defenition
+(E) Edit a Defenition
+(D) Delete a Defenition""")
