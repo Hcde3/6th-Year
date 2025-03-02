b630d7ba717639131060f854472edacc2f10a8d5
Harry Daniel <145117624+Hcde3@users.noreply.github.com>
2025-03-02 23:15:05 +0000

    Add files via upload

--- Project/analytics.py
+++ Project/analytics.py
@@ -5,41 +5,20 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-def scientific_notation(num,change_to):
-    if change_to == "float":
-        inputs = num.split("x10^")
-        power = int(inputs[1])
-        base = int(inputs[0])
-        output = base*10**power
-        return output
-    elif change_to == "scientific":
-        num = float(num)
-        integer = str(int(round(num,5)*10**5))
-        output = integer+"x10^-5"
-        return output
-
 def orderplanets(lst,element):
-    try:element_list = [scientific_notation(x[element],"float") for x in lst]
+    try:element_list = [float(x[element]) for x in lst]
     except:return False
     ordered_list = sorted(element_list)
     return ordered_list
-    
-cred = credentials.Certificate("exoplanet-dataset-firebase-adminsdk-shser-d21487840a.json")
+
+#___ Initialising Firebase ___#
+cred = credentials.Certificate("exoplanet-dataset-firebase-adminsdk-shser-369d5308fe.json")
 firebase_admin.initialize_app(cred, {'databaseURL':'https://exoplanet-dataset-default-rtdb.europe-west1.firebasedatabase.app/'})
 ref = db.reference('/')
+firebase_content = ref.get('/users/Planet Info', None)
 
-firebase_content = ref.get('/users', None)
+#___ Making Lists and Dictionaries ___#
 planetdict = firebase_content[0]["Planet Info"]
-stardict = firebase_content[0]["Star Info"]
-averages_8p = {}
-averages_7p = {}
-averages_6p = {}
-averages_5p = {}
-averages_4p = {}
-averages_3p = {}
-averages_2p = {}
-averages_1p = {}
-pnum_averages = [averages_1p,averages_2p,averages_3p,averages_4p,averages_5p,averages_6p,averages_7p,averages_8p]
 earthlike = {"Planets":{},"Averages":{}}
 superearth = {"Planets":{},"Averages":{}}
 megaearth = {"Planets":{},"Averages":{}}
@@ -50,84 +29,36 @@ browndwarf = {"Planets":{},"Averages":{}}
 outlier_planets = {"Planets":{},"Averages":{}}
 planet_categories = {"Earthlike":earthlike,"Super Earth":superearth,"Mega Earth":megaearth,"Neptunelike":neptunelike,"Jupiterlike":jupiterlike,"Hot Jupiter":hotjupiter,"Brown Dwarf":browndwarf}
 numerical_type = ["sy_snum","sy_pnum","pl_orbsmax","pl_orbper","pl_rade","pl_radj","pl_bmasse","pl_bmassj","pl_dens","pl_orbeccen","pl_insol","pl_eqt","pl_orbincl","ttv_flag","pl_imppar","pl_orblper","st_teff","st_mass","st_lum","st_logg","ra","dec","sy_dist","sy_vmag","sy_kmag","sy_gaiamag","sy_gaiamagerr1","sy_gaiamagerr2"]
-
 list_of_radii = []
 list_of_masses = [[],[],[],[],[],[],[]]
 habitablecount = {"Earthlike":0,"Super Earth":0,"Mega Earth":0}
 telescopecounts = {}
 
-#___ Planet Mass Ratio Per System Size ___#
-dist_data = [[[0,0,0,0,0,0,0,0],0],[[0,0,0,0,0,0,0],0],[[0,0,0,0,0,0],0],[[0,0,0,0,0],0],[[0,0,0,0],0],[[0,0,0],0],[[0,0],0],[[0],0]]
-test_list = [[],[],[],[],[],[],[]]
-for star,data in stardict.items():
-    planets_ordered_by_dist = []
-    for planet,pl_data in data.items():planets_ordered_by_dist.append(pl_data)
-    planets_ordered_by_dist = orderplanets(planets_ordered_by_dist,"pl_orbsmax")
-    if planets_ordered_by_dist:
-        planet_no = len(planets_ordered_by_dist)
-        for i,pd in enumerate(planets_ordered_by_dist):
-            if i == 0:
-                baseorbit = pd
-                dist_data[8-planet_no][0][0] = dist_data[8-planet_no][0][0] + 1
-            else: dist_data[8-planet_no][0][i] = dist_data[8-planet_no][0][i] + pd/baseorbit
-        dist_data[8-planet_no][1] = dist_data[8-planet_no][1] + 1
-        
-dist_averages = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0],[0,0,0,0],[0,0,0],[0,0],[0]]
-for sys_num,content in enumerate(dist_data):
-    dists = content[0]
-    times = content[1]
-    if times:
-        for i,total_dis in enumerate(dists):
-            average = total_dis/times
-            dist_averages[sys_num][i] = average
-            
-print(dist_averages)
+
+#___ Organising Data ___#       
 for planet,data in planetdict.items():
-    sy_pnum = int(scientific_notation(data["sy_pnum"],"float"))
-    planet_num_dict = pnum_averages[sy_pnum - 1]
-    for element,value in data.items():
-        if element == "pl_bmasse":
-            E_mass = scientific_notation(value,"float")
-            E_radius = scientific_notation(data["pl_rade"],"float")
-            #list_of_radii.append(E_radius)
-            #list_of_masses.append(E_mass)
-            if data.get("pl_orbper"): orbital_period = scientific_notation(data["pl_orbper"],"float")
-            else: orbital_period = False
-            if E_mass < 2 and E_radius < 2.5: earthlike["Planets"][planet] = data
-            elif E_mass < 10 and E_radius < 4: superearth["Planets"][planet] = data
-            elif E_mass < 17 and E_radius < 8:megaearth["Planets"][planet] = data
-            else:
-                if orbital_period:
-                    if orbital_period < 10: hotjupiter["Planets"][planet] = data
-                if not hotjupiter["Planets"].get(planet):
-                    if E_mass < 125: neptunelike["Planets"][planet] = data
-                    elif E_mass > 125 and scientific_notation(data["pl_bmassj"],"float") < 75:jupiterlike["Planets"][planet] = data
-                    else: browndwarf["Planets"][planet] = data
-        if element in numerical_type:
-            if value:
-                value = scientific_notation(value,"float")
-                if planet_num_dict.get(element):
-                    planet_num_dict[element] = (value+planet_num_dict[element][0],planet_num_dict[element][1]+1)
-                else:
-                    planet_num_dict[element] = (value,1)
+    sy_pnum = int(data["sy_pnum"])
+    E_mass = data["pl_bmasse"]
+    E_radius = data["pl_rade"]
+    if data.get("pl_orbper"): orbital_period = data["pl_orbper"]
+    else: orbital_period = False
+    if E_mass < 2 and E_radius < 2.5: earthlike["Planets"][planet] = data
+    elif E_mass < 10 and E_radius < 4: superearth["Planets"][planet] = data
+    elif E_mass < 17 and E_radius < 8:megaearth["Planets"][planet] = data
+    else:
+        if orbital_period:
+            if orbital_period < 10: hotjupiter["Planets"][planet] = data
+        if not hotjupiter["Planets"].get(planet):
+            if E_mass < 125: neptunelike["Planets"][planet] = data
+            elif E_mass > 125 and data["pl_bmassj"] < 75:jupiterlike["Planets"][planet] = data
+            else: browndwarf["Planets"][planet] = data
                     
-        #___ Tallying up Telescope Discoveries __#
-        if element == "disc_telescope":
-            if telescopecounts.get(value): telescopecounts[value] = telescopecounts[value] + 1
-            else: telescopecounts[value] = 1
-            
-for index, sysize_dict in enumerate(pnum_averages):
-    planet_no = index + 1
-    print(f"\n\n{planet_no} Planets:")
-    for element in sysize_dict:
-        total = sysize_dict[element][0]
-        values = sysize_dict[element][1]
-        sysize_dict[element] = total/values
-        if element == "sy_pnum": total_pnum = values
-        print(f"-----average {element} is {sysize_dict[element]}")
-    print(f"Data gathered from {total_pnum} planets across {int(total_pnum/planet_no)} stars.")
+#___ Tallying up Telescope Discoveries ___#
+    telescope = data["disc_telescope"]
+    if telescopecounts.get(telescope): telescopecounts[telescope] = telescopecounts[telescope] + 1
+    else: telescopecounts[telescope] = 1
 
-
+#___ Analysing and Finding Averages ___#
 for i,category in enumerate(planet_categories):
     planets = planet_categories[category]["Planets"]
     averages = planet_categories[category]["Averages"]
@@ -135,21 +66,21 @@ for i,category in enumerate(planet_categories):
         for element,value in data.items():
             if element in numerical_type:
                 if value:
-                    value = scientific_notation(value,"float")
                     if averages.get(element):
                         averages[element] = (value+averages[element][0],averages[element][1]+1)
                     else:
                         averages[element] = (value,1)
             if element == "pl_bmasse":list_of_masses[i].append(math.log10(value))
-            #___ Checking for Habitability ___# assumptions made: Temperature to support water 0-100C and using 60C as an upper limit for temp, Star must live long enough to allow life to form (star mass < 1.5 suns) and must be big enough to allow a viable distance between star and planet (star mass > 0.6 suns a.k.a not a Red Dwarf)            
+#___ Checking for Habitability ___# assumptions made: Temperature to support water 0-100C and using 60C as an upper limit for temp, Star must live long enough to allow life to form (star mass < 1.5 suns) and must be big enough to allow a viable distance between star and planet (star mass > 0.6 suns a.k.a not a Red Dwarf)            
             if element == "pl_eqt" and value:
                 if value > 0+273.15 and value < 60+273.15:
                     try:
-                        star_mass = scientific_notation(data["st_mass"],"float")
+                        star_mass = data["st_mass"]
                         if star_mass <= 1.5 and star_mass >= 0.6:
                             habitablecount[category] = habitablecount[category] + 1
                     except:pass
-                    
+    
+    #______ Printing Averages ______#
     print(f"\n\n{category} Planets:")
     for element in averages:
         total = averages[element][0]
@@ -195,9 +126,7 @@ tels = []
 counts = []
 othercount = 0
 for telescope,count in telescopecounts.items():
-    if count > 50:
-        #if "*" in telescope:
-            
+    if count > 50:  
         tels.append(telescope)
         counts.append(count)
     else:
@@ -231,15 +160,6 @@ plot.xlabel('Planet Types')
 plot.ylabel('Average Earth Radii')
 plot.show()
 Analytics["Graph_Info"]["Radii"] = list_of_radii
-
-#___ Bar Chart of Orbital Radius compared to First ___#
-sys_size = int(input("Choose what size solar system to compare orbits of. (1-7)planets:\n"))
-plot.bar([1,2,3,4,5,6,7][:sys_size],dist_averages[8-sys_size])
-plot.title('Orbital Distance Compared to First')
-plot.xlabel('Orbital Position')
-plot.ylabel('Orbital Distance / First Orbit')
-plot.show()
-Analytics["Graph_Info"]["Orbital Radii"] = dist_averages
 
 #___ Transferring Data to FireBase ___#
 ref = db.reference('/Analytics')
--- Project/data cleaning & reading.py
+++ Project/data cleaning & reading.py
@@ -3,14 +3,13 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("exoplanet-dataset-firebase-adminsdk-shser-d21487840a.json")
+cred = credentials.Certificate("exoplanet-dataset-firebase-adminsdk-shser-369d5308fe.json")
 firebase_admin.initialize_app(cred, {'databaseURL':'https://exoplanet-dataset-default-rtdb.europe-west1.firebasedatabase.app/'})
 ref = db.reference('/')
 
 def dataclean(value): #removes $,#,[,],/ or . from string and converts floats to scientific notion
     try:
-        float(value)
-        cleaned = scientific_notation(value,"scientific")
+        cleaned = float(value)
     except:
         cleaned = ""
         for char in value:
@@ -34,19 +33,6 @@ def typecheck(d,i):
     if not d: valid = False
     return valid
 
-def scientific_notation(num,change_to):
-    if change_to == "float":
-        inputs = num.split("x10^")
-        power = int(inputs[1])
-        base = int(inputs[0])
-        output = base*10**power
-        return output
-    elif change_to == "scientific":
-        num = float(num)
-        integer = str(int(round(num,5)*10**5))
-        output = integer+"x10^-5"
-        return output
-
 def SphereAverageDensity(rad,mass):
     volume = math.pi*(4/3)*(rad**3)
     density = mass/volume
@@ -62,7 +48,6 @@ with open("exoplanet dataset.csv","r") as dataset:
     string_type = ["pl_name","hostname","disc_facility","disc_telescope","st_spectype","rastr","decstr","pl_bmassprov"]
     to_be_cleaned = {}
     pl_clean = {}
-    stars = {}
     
     for line in dataset:
         
@@ -84,15 +69,8 @@ with open("exoplanet dataset.csv","r") as dataset:
             else:pl_name = data
            
         #___ Pure info being stored and impure being noted ___#        
-        if valid:
-            pl_clean[pl_name] = pl_dict
-            
-            #___ Star Dict ___#
-            c_star = pl_dict["hostname"]
-            if not stars.get(c_star): stars[c_star] = {}     
-            stars[c_star][pl_name] = pl_dict
-        else:
-            to_be_cleaned[pl_name] = pl_dict
+        if valid:pl_clean[pl_name] = pl_dict
+        else:to_be_cleaned[pl_name] = pl_dict
 
 #___ Finding Missing Values ___#
 missingvaluecounts = {}
@@ -103,10 +81,10 @@ for impurity_pl, impure_set in to_be_cleaned.items():
         
         #__ Calculating missing densities__#
         if not impure_set["pl_dens"] and (impure_set["pl_bmasse"] and impure_set["pl_rade"]):
-            radius_in_cm = scientific_notation(impure_set["pl_rade"],"float")*100*6*10**6
-            mass_in_g = scientific_notation(impure_set["pl_bmasse"],"float")*1000*6*10**24
+            radius_in_cm = impure_set["pl_rade"]
+            mass_in_g = impure_set["pl_bmasse"]
             calculated_density = SphereAverageDensity(radius_in_cm,mass_in_g)
-            impure_set["pl_dens"] = scientific_notation(calculated_density,"scientific")    
+            impure_set["pl_dens"] = calculated_density
         
         #__ Measuring missing data __#
         if not impure_set[column]:
@@ -115,15 +93,11 @@ for impurity_pl, impure_set in to_be_cleaned.items():
             if column in essentialcolumns: useable = False
             else: pass
             
-    if useable:
-        pl_clean[impurity_pl] = impure_set
-        c_star = impure_set["hostname"]
-        if not stars.get(c_star): stars[c_star] = {}      
-        stars[c_star][impurity_pl] = impure_set
+    if useable:pl_clean[impurity_pl] = impure_set
         
 [print(f"Column {i} is missing {k} values.") for i,k in missingvaluecounts.items()]
 #___ Adding to firebase ___#
-Celestial_info = {"Star Info": stars, "Planet Info": pl_clean}
+Celestial_info = {"Planet Info": pl_clean}
 ref.set(Celestial_info)
 
 
